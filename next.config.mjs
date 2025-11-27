c587a6403bb680508b428cc0e43d8642f0815963
mahabubDevs <mdmahabubrahman800@gmail.com>
2025-11-27 09:04:44 +0600

    update redireact

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',           // সব route capture করবে
+        has: [{ type: 'host', value: 'mrbitx.com' }], // non-www
+        destination: 'https://www.mrbitx.com/:path*', // www তে redirect
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
